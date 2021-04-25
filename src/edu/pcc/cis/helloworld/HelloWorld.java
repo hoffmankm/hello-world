993a780a8656f230bd51a94381c38fb77974b6ef
Kevin Hoffman <kevin.hoffman15@pcc.edu>
2021-04-25 12:18:09 -0700

    print date

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.pcc.cis.helloworld;
 
+import java.util.Date;
+
 /**
  * Prints the classic 'Hello World' message
  *
@@ -11,5 +13,6 @@ package edu.pcc.cis.helloworld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println(new Date());
     }
 }
