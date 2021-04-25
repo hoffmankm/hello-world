7490e0aa7c33cdab39a9992e761f12beaad8406a
Kevin Hoffman <kevin.hoffman15@pcc.edu>
2021-04-25 12:15:03 -0700

    added main method that prints hello world

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -1,4 +1,15 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Prints the classic 'Hello World' message
+ *
+ * @author Kevin Hoffman
+ * @version 2021.04.25
+ *
+ */
+
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
